08325e85cf083dc7b6c14e06ccf328a4fe205c0b
Hoon94 <dleognsdl1@naver.com>
2024-06-07 17:09:01 +0900

    feat: Project 1, part one

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Daehoon Lee on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
