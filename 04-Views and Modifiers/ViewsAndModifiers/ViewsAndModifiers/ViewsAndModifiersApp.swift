bcd840b8354f0d67f0653301c1b472bf6954139f
Hoon94 <dleognsdl1@naver.com>
2024-06-14 23:18:25 +0900

    feat: Project 3, part one

---
+++ 04-Views and Modifiers/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Daehoon Lee on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
