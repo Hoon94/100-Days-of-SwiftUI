42834e30b0c3baf54427a674a8e03cff92ec6413
Hoon94 <dleognsdl1@naver.com>
2024-06-17 15:38:01 +0900

    feat: Project 4, part one

---
+++ 06-BetterRest/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Daehoon Lee on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
