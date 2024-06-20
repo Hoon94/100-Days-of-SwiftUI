f4cbc581fca4c8b490c451025129f02f1999ca56
Hoon94 <dleognsdl1@naver.com>
2024-06-20 23:43:34 +0900

    feat: Project 5, part one

---
+++ 07-WordScramble/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Daehoon Lee on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
