b8c3f8c17b7df6be35735a7d06af3c30a5c91b32
Hoon94 <dleognsdl1@naver.com>
2024-06-23 23:34:00 +0900

    feat: Project 6, part one

---
+++ 08-Animations/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Daehoon Lee on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
