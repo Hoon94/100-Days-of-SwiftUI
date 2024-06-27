7fb1fc3b497524a4963ebbcc09b5a2fcbb864cf8
Hoon94 <dleognsdl1@naver.com>
2024-06-27 23:38:04 +0900

    feat: Project 7, part one

---
+++ 10-iExpense/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Daehoon Lee on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
