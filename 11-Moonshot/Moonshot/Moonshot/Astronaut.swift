e664a80488744993f4e797f89a552e43d25bfa96
Hoon94 <dleognsdl1@naver.com>
2024-07-01 16:15:09 +0900

    feat: Project 8, part two

---
+++ 11-Moonshot/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Daehoon Lee on 7/1/24.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
