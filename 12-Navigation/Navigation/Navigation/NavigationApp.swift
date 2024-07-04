3a4b873a3e9388e5baa7e62d83dd6993e48a4d9b
Hoon94 <dleognsdl1@naver.com>
2024-07-04 10:45:32 +0900

    feat: Project 9, part one

---
+++ 12-Navigation/Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Daehoon Lee on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
