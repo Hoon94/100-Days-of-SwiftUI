799ccd18071ab01263cdbda8685520c966006c6f
Hoon94 <dleognsdl1@naver.com>
2024-07-10 23:49:33 +0900

    feat: Project 10, part one

---
+++ 14-Cupcake Corner/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Daehoon Lee on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
