60deda9f59fa8a588ee03e7629870afba56abdc7
Hoon94 <dleognsdl1@naver.com>
2024-07-12 23:56:32 +0900

    feat: Project 10, part two

---
+++ 14-Cupcake Corner/CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -0,0 +1,20 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Daehoon Lee on 7/12/24.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var order: Order
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}
